46c8bca1f408af4ee31ab8920b381dca8eab56a1
Kevin <magneticok@gmail.com>
2019-08-20 13:44:12 -0600

    Auto stash before revert of "Segunda fase"

--- Preparacion2/src/logica/Snake.java
+++ Preparacion2/src/logica/Snake.java
@@ -1,4 +1,17 @@
 package logica;
 
+import java.util.List;
+
+/**
+ * Clase que representa la serpiente.
+ * Va a necesitar:
+ *   Atributos:
+ *     Su Largo
+ *     Si esta Viva
+ *     Los puntos que la conforman
+ *     Una referencia a la cabeza
+ *     Su velocidad en X y en Y
+ *     
+ * */
 public class Snake {
 }
