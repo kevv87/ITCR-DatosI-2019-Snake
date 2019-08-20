8f9f544569a83b5cbf0ca7662b2fb2e6b5d505f1
Kevin <magneticok@gmail.com>
2019-08-20 13:42:13 -0600

    Revert "Segunda fase"
    This reverts commit 99d44aa837928868e8ec5bfd7b2946412ee7ef2e.

--- Preparacion2/src/logica/Snake.java
+++ Preparacion2/src/logica/Snake.java
@@ -1,15 +1,4 @@
 package logica;
 
-/**
- * Clase que representa la serpiente.
- * Va a necesitar:
- *   Atributos:
- *     Su Largo
- *     Si esta Viva
- *     Los puntos que la conforman
- *     Una referencia a la cabeza
- *     Su velocidad en X y en Y
- * */
 public class Snake {
-
 }
